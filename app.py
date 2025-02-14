d04e09e033aa50eb3d898c00b6913c710f9cfb15
vladpochuev <pochuev.vladislav@gmail.com>
2025-02-14 16:07:31 +0200

    Add SQLAlchemy models

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 from os.path import join, dirname
 
+import telebot
 from dotenv import load_dotenv
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-import telebot
 
 
 def get_from_env(key):
@@ -26,6 +26,29 @@ server_url = f"{get_from_env("SERVER_URL")}/webhook"
 bot.set_webhook(url=server_url)
 
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    user_name = db.Column(db.String())
+    language_code = db.Column(db.String())
+    registered_at = db.Column(db.DateTime, nullable=False)
+
+
+class Topic(db.Model):
+    __tablename__ = "topics"
+
+    topic_name = db.Column(db.String(), primary_key=True)
+    value = db.Column(db.String())
+
+
+user_topics = db.Table("users_topics",
+                       db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+                       db.Column("topic_name", db.String(), db.ForeignKey("topics.topic_name"), primary_key=True))
+
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
     json_string = request.get_data().decode("utf-8")
@@ -39,6 +62,9 @@ def start_command(message):
     bot.send_message(message.chat.id, "Start command")
     return {"ok": True}
 
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run()
